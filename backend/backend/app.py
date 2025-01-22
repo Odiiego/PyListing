e1ed3fc39fe28a5555897530049fa6f9eceda7e4
Diego <odiegodev@gmail.com>
2025-01-22 06:34:54 -0300

    feat: configuração Docker e PostgreSQL

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -5,7 +5,4 @@ app = FastAPI()
 
 @app.get('/', response_model=str)
 def read_root():
-    return """PyListing é um aplicativo desenvolvido
-            com FastAPI que facilita a gestão de listas
-            de compras enquanto ajuda você a comparar
-            marcas e encontrar os melhores preços."""
+    return 'https://github.com/Odiiego/PyListing'
