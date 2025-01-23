e856b644c3b46775c98d98738052ceb8bf6d410d
Diego <odiegodev@gmail.com>
2025-01-23 04:24:48 -0300

    feat: adicionei as rotas para autenticação e de manipulação de usuários

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from backend.routers import auth, users
+
 app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get('/', response_model=str)
---
+++ backend/backend/routers/auth.py
@@ -0,0 +1,51 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from backend.database import get_session
+from backend.models import User
+from backend.schemas import Token
+from backend.security import (
+    create_access_token,
+    get_current_user,
+    verify_password,
+)
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+OAutg2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+T_Session = Annotated[Session, Depends(get_session)]
+
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(form_data: OAutg2Form, session: T_Session):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user.username})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.post('/refresh_token', response_model=Token)
+def refresh_access_token(
+    user: User = Depends(get_current_user),
+):
+    new_access_token = create_access_token(data={'sub': user.username})
+
+    return {'access_token': new_access_token, 'token_type': 'bearer'}
