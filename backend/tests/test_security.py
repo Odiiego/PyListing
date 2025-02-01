f1745ef2c2c3e99d59b83475f3be1a9b5c481bef
Diego <odiegodev@gmail.com>
2025-01-31 21:49:19 -0300

    test: adicionei testes as rotas de auth e security

---
+++ backend/tests/test_security.py
@@ -0,0 +1,49 @@
+from http import HTTPStatus
+
+from jwt import decode
+
+from backend.security import create_access_token, settings
+
+
+def test_create_jwt():
+    data = {'test': 'test'}
+    token = create_access_token(data)
+
+    decoded = decode(
+        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
+
+    assert decoded['test'] == data['test']
+    assert decoded['exp']
+
+
+def test_req_with_invalid_jwt(client):
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': 'Bearer invalid-token'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_jwt_without_credentials(client, user):
+    token = create_access_token({})
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_create_jwt_with_invalid_email(client, user):
+    token = create_access_token({'sub': 'invalid-email'})
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
