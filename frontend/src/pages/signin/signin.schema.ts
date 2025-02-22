75d5cae842d760490261653f4112b7d35a4dedd6
Diego <odiegodev@gmail.com>
2025-02-22 00:17:43 -0300

    fix: pequeno ajuste temporário no schema das senhas

--- frontend/src/pages/signin/signin.schema.ts
+++ frontend/src/pages/signin/signin.schema.ts
@@ -5,9 +5,9 @@ export const signInSchema = z.object({
   password: z
     .string()
     .min(6, 'A senha deve ter no mínimo 6 caracteres')
-    .max(20, 'A senha deve ter no máximo 20 caracteres')
-    .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
-    .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
-    .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
-    .regex(/[\W_]/, 'A senha deve conter pelo menos um caractere especial'),
+    .max(20, 'A senha deve ter no máximo 20 caracteres'),
+  // .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
+  // .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
+  // .regex(/[0-9]/, 'A senha deve conter pelo menos um número')
+  // .regex(/[\W_]/, 'A senha deve conter pelo menos um caractere especial'),
 });
