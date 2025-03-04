f3e1d69faa6176f86ee9500281f20c026cbaecd8
Diego <odiegodev@gmail.com>
2025-03-03 23:43:29 -0300

    feat: adiciona a interface de produtos

--- frontend/src/services/lists/listServices.type.ts
+++ frontend/src/services/lists/listServices.type.ts
@@ -1,7 +1,10 @@
+import { IProduct } from '../products/products.type';
+
 export interface IShoppingList {
   id: number;
   user_id: number;
   name: string;
+  products: IProduct[];
   created_at: string;
   updated_at: string;
 }
---
+++ frontend/src/services/products/products.type.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+  id: number;
+  list_id: number;
+  name: string;
+  quantity: number;
+  brands: [];
+  best_price: string;
+  best_offer: string;
+  created_at: string;
+  updated_at: string;
+}
