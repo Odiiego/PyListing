8cd71f2b6cca7548d9b5ca11f57c44c3efec675a
Diego <odiegodev@gmail.com>
2025-03-07 02:31:12 -0300

    feat: adiciona o caso de uso de criação de marcas

---
+++ frontend/src/services/brands/brandServices.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { ICreateBrandSchemaType } from '../../components/product/product.type';
+import { IToken } from '../auth/authService.type';
+import { IBrand } from './brandServices.type';
+
+export const createBrandService = async (
+  data: ICreateBrandSchemaType,
+  productId: undefined | number,
+  userToken: undefined | IToken,
+): Promise<IBrand> => {
+  const response = await axios.post(
+    `http://localhost:8000/brands/${productId}`,
+    data,
+    {
+      headers: {
+        Authorization: `Bearer ${userToken?.access_token}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  return response.data;
+};
---
+++ frontend/src/services/brands/brandServices.type.ts
@@ -0,0 +1,11 @@
+export interface IBrand {
+  id: number;
+  product_id: number;
+  quantity: number;
+  name: string;
+  price: number;
+  unity_cost: number;
+  predicted_cost: number;
+  created_at: string;
+  updated_at: string;
+}
