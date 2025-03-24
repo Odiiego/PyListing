2b2f790eabc77a7597a624d2073ba3eb9f5c1a6c
Diego <odiegodev@gmail.com>
2025-03-24 01:59:40 -0300

    feat: adiciona o caso de uso para buscar uma marca

--- frontend/src/services/brands/brandServices.ts
+++ frontend/src/services/brands/brandServices.ts
@@ -24,6 +24,25 @@ export const createBrandService = async (
   }
 };
 
+export const getBrandsService = async (
+  productId: number,
+  userToken: undefined | IToken,
+): Promise<{ brands: IBrand[] }> => {
+  try {
+    if (!userToken) throw new Error('Token de usuário não encontrado');
+    if (!productId) throw new Error('ID do produto não pode ser indefinido');
+
+    const response = await api.get(`/brands/product/${productId}`, {
+      headers: getAuthHeaders(userToken),
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar a marca:', error);
+    throw new Error(error instanceof Error ? error.message : 'Erro inesperado');
+  }
+};
+
 export const deleteBrandService = async (
   brandId: number,
   userToken: undefined | IToken,
